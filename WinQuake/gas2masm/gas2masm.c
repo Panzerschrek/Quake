5b0095d23ca63166abd0dcb079a2b1fcafc724fb
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-07-14 21:16:16 +0700

    Projekt skonvertirovan v format VisualStudio 2008. Pocineny ošibki sborki.

--- WinQuake/gas2masm/gas2masm.c
+++ WinQuake/gas2masm/gas2masm.c
@@ -21,8 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // gas to MASM source code converter
 //
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #define MAX_TOKENS			100
