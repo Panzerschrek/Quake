d038f0792475d399a1f933abc7b48ff9c2dc4ff6
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-07-26 15:36:26 +0700

    Uveliceny predely na razmer ekrana.

--- WinQuake/d_polyse.c
+++ WinQuake/d_polyse.c
@@ -24,8 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "r_local.h"
 #include "d_local.h"
 
-// TODO: put in span spilling to shrink list size
-// !!! if this is changed, it must be changed in d_polysa.s too !!!
 #define DPS_MAXSPANS			MAXHEIGHT+1	
 									// 1 extra for spanpackage that marks end
 
