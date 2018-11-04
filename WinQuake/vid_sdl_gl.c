06497c41c1b396ac1ff086f034941a696404abea
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-11-04 18:31:23 +0700

    Zaprašivajem u OpenGL alfa-kanal.

--- WinQuake/vid_sdl_gl.c
+++ WinQuake/vid_sdl_gl.c
@@ -465,6 +465,7 @@ void	VID_Init (unsigned char *palette)
 
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
 
 	if (msaa_samples != 0)
 	{
